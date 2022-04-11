543527b70f8b9d134f880abc211c506e5ef30562
User <a>
2022-04-12 02:35:03 +0300

    added second task

---
+++ src/main/java/ru/gb/dziachuk/first_task/SecondaTask.java
@@ -0,0 +1,15 @@
+package ru.gb.dziachuk.first_task;
+
+public class SecondaTask {
+
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+}
---
+++ src/main/java/ru/gb/dziachuk/homeworc2/Task.java
@@ -0,0 +1,7 @@
+package ru.gb.dziachuk.homeworc2;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("ghj");
+    }
+}
