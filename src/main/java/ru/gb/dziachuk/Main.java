c63bd5ef384363c3898360392eff0ed5e0c23a16
User <a>
2022-04-12 01:20:40 +0300

    first task

--- src/main/java/ru/gb/dziachuk/Main.java
+++ src/main/java/ru/gb/dziachuk/Main.java
@@ -1,14 +1,7 @@
 package ru.gb.dziachuk;
 
 public class Main {
-
-    public static void printThreeWords(){
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
-    }
-
     public static void main(String[] args) {
-        printThreeWords();
+
     }
 }
