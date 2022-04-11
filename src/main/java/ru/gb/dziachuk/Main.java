b62ca8e1c122d2293af97b849bd5429ec1e0c6e6
User <a>
2022-04-12 01:19:28 +0300

    second task

--- src/main/java/ru/gb/dziachuk/Main.java
+++ src/main/java/ru/gb/dziachuk/Main.java
@@ -1,7 +1,14 @@
 package ru.gb.dziachuk;
 
 public class Main {
-    public static void main(String[] args) {
 
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
     }
 }
